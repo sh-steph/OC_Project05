cfdb549d32461c1b11c3df20eb3a2903675c290a
Stephane Hua <>
2023-07-20 22:09:39 +0200

    Remove unused folders

--- src/main/java/com/openclassrooms/starterjwt/repository/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openclassrooms.starterjwt.repository;
-
-import com.openclassrooms.starterjwt.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository  extends JpaRepository<Session, Long> {
-}
