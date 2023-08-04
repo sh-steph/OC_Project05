6eb09ad665fa5f7071f77d5549ef8c53fdf865e2
Stephane Hua <>
2023-08-04 17:11:09 +0200

    #2.1: Update cypress and add 2 tests E2E (Test Login connection and Error on required field)

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,50 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login')
+  beforeEach(() => {
+    cy.visit('/login');
+  });
 
+  it('should login successfully', () => {
     cy.intercept('POST', '/api/auth/login', {
       body: {
         id: 1,
         username: 'userName',
         firstName: 'firstName',
         lastName: 'lastName',
-        admin: true
+        admin: true,
       },
-    })
+    });
 
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []
+    ).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
+
+  it('should not login if field is invalid', () => {
+
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type('fakePassword');
+    cy.get('[type="submit"]').click();
+
+    cy.get('.error').should('contain', 'An error occurred');
+
+    cy.get('input[formControlName=email]').focus().clear();
+    cy.get('input[formControlName=password]').focus().clear();
+
+    cy.get('input[formControlName=email]').type('fakeMail@fake.com');
+    cy.get('input[formControlName=password]').type('test!1234');
+    cy.get('[type="submit"]').click();
+
+    cy.get('.error').should('contain', 'An error occurred');
+  });
 });
