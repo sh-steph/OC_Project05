b2c01caf69634bb9b7bdc4984570c5aa77b2feab
Stephane <stephane.hua1997@gmail.com>
2023-08-29 22:55:27 +0200

    #3.4: Add 2 integ test (register and login)

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -1,8 +1,13 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -10,27 +15,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.text.ParseException;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest()
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 class AuthControllerTest {
-
     @Autowired
     private MockMvc mvc;
+    User user;
+
+    @BeforeEach
+    public void setup() throws ParseException {
+
+        user = User.builder()
+                .id(2L)
+                .email("User1@test.com")
+                .lastName("lastNameTest")
+                .firstName("firstNameTest")
+                .password("Test1234-")
+                .admin(true)
+                .build();
+    }
 
     @Test
     public void registerUserTest() throws Exception {
         SignupRequest signupRequest = new SignupRequest();
-        signupRequest.setEmail("test1@test.com");
-        signupRequest.setFirstName("John1");
-        signupRequest.setLastName("Doe1");
-        signupRequest.setPassword("Test1234-1");
+        signupRequest.setEmail(user.getEmail());
+        signupRequest.setFirstName(user.getFirstName());
+        signupRequest.setLastName(user.getLastName());
+        signupRequest.setPassword(user.getPassword());
         mvc.perform(MockMvcRequestBuilders
                         .post("/api/auth/register")
                         .content(asJsonString(signupRequest))
@@ -38,7 +63,23 @@ class AuthControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
-    // detruire cette donnée
+
+    @Test
+    public void authenticateUserTest() throws Exception {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail(user.getEmail());
+        loginRequest.setPassword(user.getPassword());
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/auth/login")
+                        .content(asJsonString(loginRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        loginRequest.getEmail();
+    }
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
@@ -46,4 +87,13 @@ class AuthControllerTest {
             throw new RuntimeException(e);
         }
     }
+
+//    @Test
+//    public void deleteRegisterTest() throws Exception {
+//        mvc.perform(MockMvcRequestBuilders
+//                        .delete("/api/user/{userId}", 15)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
 }
