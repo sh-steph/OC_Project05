28ad1515f946f46e9476539b3d447e566a04480d
Stephane <stephane.hua1997@gmail.com>
2023-08-31 23:40:43 +0200

    #3.5 : add Builder annotation in SessionDto

--- back/src/main/java/com/openclassrooms/starterjwt/dto/SessionDto.java
+++ back/src/main/java/com/openclassrooms/starterjwt/dto/SessionDto.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.starterjwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SessionDto {
     private Long id;
 
